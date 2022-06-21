9c4b9a7963022b725aa44bdb00bf425c5cf919f0
Oluwanifemi Kolawole <gracekolawole@fb.com>
2022-06-21 11:25:49 -0700

    finished generating the keys and user sigh-in using OAuth

---
+++ twitter/View Controllers/TimelineViewController.m
@@ -0,0 +1,65 @@
+//
+//  TimelineViewController.m
+//  twitter
+//
+//  Created by emersonmalca on 5/28/18.
+//  Copyright © 2018 Emerson Malca. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import "TimelineViewController.h"
+#import "APIManager.h"
+#import "AppDelegate.h"
+//#import "Tweet.h"
+
+@interface TimelineViewController ()
+
+@end
+
+@implementation TimelineViewController
+- (IBAction)didTapLogout:(id)sender {
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+//    [UIApplication sharedApplication].delegate;
+    [[APIManager shared] logout];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+//  Get timeline
+    [[APIManager shared] getHomeTimelineWithCompletion:^(NSArray *tweets, NSError *error) {
+        if (tweets) {
+            NSLog(@"😎😎😎 Successfully loaded home timeline");
+            for (NSDictionary *dictionary in tweets) {
+                NSString *text = dictionary[@"text"];
+                NSLog(@"%@", text);
+            }
+        } else {
+            NSLog(@"😫😫😫 Error getting home timeline: %@", error.localizedDescription);
+        }
+//        [[APIManager shared] logout];
+    }];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
